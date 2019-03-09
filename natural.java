d41b0854caef96d1656df4837fb4a148ac13e411
archana <47376756+sappathi1999@users.noreply.github.com>
2019-03-08 21:30:53 -0800

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int num=3,i=1,sum=0;
+	while(i<=num)
+	{
+		sum+=i;
+		i++;
+	}
+	System.out.println("sum="+sum);
+	}
+}
+
